0a538120343d45b3a2198af6945446903c0a3b32
unknown <mauriciodolacio@outlook.com>
2022-09-27 19:52:57 -0300

    Aula 3

--- bin/aluradart.dart
+++ bin/aluradart.dart
@@ -26,4 +26,33 @@ void main() {
   const int varConst = 20; //o valor desta variavel não pode ser mudado
   final String varFinal; //essa variavel só pode ser alterada uma vez.
   varFinal = 'final'; //tentar fazer mais alterações irá causar um erro.
+
+  //Aula 3: 5 métodos de lista
+  List<String> list = ["Ricarth", "Ruan", "da", "Silva", "Lima"];
+
+  //sublist faz uma sublista da lista original
+  List<String> sublist = list.sublist(1, 3);
+  print(sublist);
+
+  //forEach: percorre a lista inteira como um for
+  list.forEach((element) {
+    print(element);
+  });
+  print('Fim da lista!');
+
+  //contains: verifica se a lista possui algum conteudo especificado
+  String procurando = "Silva";
+  if (list.contains(procurando)) {
+    print('Encontrado');
+  }
+
+  //reduce: reduz a lista a um objeto
+  String myName = list.reduce((value, element) {
+    return value + " " + element;
+  });
+  print(myName);
+
+  //where
+  Iterable maior = list.where((element) => element.length > 4);
+  print(maior);
 }
