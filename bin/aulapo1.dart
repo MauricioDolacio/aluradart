e9ec55a60e154ac63d88b2081e053598ad785d43
unknown <mauriciodolacio@outlook.com>
2022-09-27 22:15:06 -0300

    Aula 02 PO: Classes

--- bin/aulapo1.dart
+++ bin/aulapo1.dart
@@ -4,25 +4,15 @@ import 'package:aluradart/aluradart.dart' as aluradart;
 
 void main() {
   String nome = "Uva";
+  double peso = 100.2;
   String cor = 'Roxo';
+  String sabor = 'Doce';
   int diasDesdeColheita = 40;
   bool isMadura = funcEstaMadura(diasDesdeColheita);
 
-  print(isMadura);
-  mostraMadura(nome: 'Uva', dias: 40, cor: cor);
-}
-
-// Posicionais Obrigatórios
-// Nomeados Opcionais
-// Parâmetros com "Padrão"
-// Modificador "required"
-mostraMadura({String? nome, required int dias, String cor = 'Sem cor'}) {
-  if (dias >= 30) {
-    print("A $nome está madura");
-  } else {
-    print("A $nome não está madura");
-  }
-  print('A $nome é $cor');
+  //Objetos
+  Fruta fruta01 = Fruta(nome, peso, cor, sabor, diasDesdeColheita);
+  Fruta fruta02 = Fruta('Laranja', 40, 'Laranja', 'Citrica', 20);
 }
 
 //Função
@@ -31,5 +21,40 @@ bool funcEstaMadura(int dias) {
     return true;
   } else {
     return false;
+  }
+}
+
+//Parâmetros diferentes
+mostraMadura({String? nome, required int dias, String cor = 'Sem cor'}) {
+  // Posicionais Obrigatórios
+// Nomeados Opcionais
+// Parâmetros com "Padrão"
+// Modificador "required"
+  if (dias >= 30) {
+    print("A $nome está madura");
+  } else {
+    print("A $nome não está madura");
+  }
+  print('A $nome é $cor');
+}
+
+//Classe (Objeto)
+class Fruta {
+  String nome;
+  double peso;
+  String cor;
+  String sabor;
+  int diasDesdeColheita;
+  bool? isMadura;
+
+  //Construtor (Parâmetros da classe)
+  Fruta(this.nome, this.peso, this.cor, this.sabor, this.diasDesdeColheita,
+      {this.isMadura});
+
+  //Métodos (funções da classe)
+  void estaMadura(int diasParaMadura) {
+    isMadura = diasDesdeColheita >= diasParaMadura;
+    print(
+        'A sua $nome foi colhida a $diasDesdeColheita dias, e precisa de $diasParaMadura para poder comer. Ela esta madura? $isMadura');
   }
 }
